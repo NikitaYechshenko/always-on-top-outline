e5059b712d140f5db91ee85494e327ed078579eb
nikitayechshenko <nikitayech@gmail.com>
2026-03-31 14:29:50 +0500

    init

--- extension.js
+++ extension.js
@@ -13,38 +13,58 @@ export default class AlwaysOnTopIndicatorExtension extends Extension {
     }
 
     enable() {
-        // Initialize settings
-        this._settings = this.getSettings();
-        this._borderWidth = this._settings.get_double('border-thickness');
-        
+        // Initialize settings - try/catch to provide defaults if schema not found
+        try {
+            this._settings = this.getSettings('org.gnome.shell.extensions.always-on-top-outline');
+            this._borderWidth = this._settings.get_double('border-thickness');
+            this._borderColor = this._settings.get_string('border-color');
+            this._borderAlpha = this._settings.get_double('border-alpha');
+        } catch (e) {
+            log('[Always-On-Top] Settings error, using defaults:', e);
+            // Use default values if settings fail
+            this._settings = null;
+            this._borderWidth = 2.0;
+            this._borderColor = 'bd93f9';
+            this._borderAlpha = 1.0;
+        }
+
         // Watch for settings changes
-        this._settingsChangedId = this._settings.connect('changed::border-thickness', () => {
-            const newWidth = this._settings.get_double('border-thickness');
-            if (newWidth === this._borderWidth) return; // No change
-            
-            this._borderWidth = newWidth;
-            
-            // Update all existing borders - just update their style, don't recreate
-            this._borders.forEach((borderInfo, metaWindow) => {
-                if (borderInfo.actor) {
-                    borderInfo.actor.set_style(`border: ${this._borderWidth}px solid rgba(189, 147, 249, 1);
-                                                background-color: transparent;`);
-                    // Update position to account for new border width
-                    try {
-                        const rect = metaWindow.get_frame_rect();
-                        borderInfo.actor.set_position(rect.x - this._borderWidth, rect.y - this._borderWidth);
-                        borderInfo.actor.set_size(rect.width + 2 * this._borderWidth, rect.height + 2 * this._borderWidth);
-                    } catch (e) {
-                        // Window may have been destroyed
-                    }
-                }
+        if (this._settings) {
+            this._settingsChangedId = this._settings.connect('changed::border-thickness', () => {
+                const newWidth = this._settings.get_double('border-thickness');
+                if (newWidth === this._borderWidth) return; // No change
+
+                this._borderWidth = newWidth;
+
+                // Update all existing borders - recreate them with new thickness
+                this._borders.forEach((borderInfo, metaWindow) => {
+                    this._removeBorder(metaWindow);
+                    this._addBorder(metaWindow);
+                });
             });
-        });
-        
+
+            this._colorChangedId = this._settings.connect('changed::border-color', () => {
+                const newColor = this._settings.get_string('border-color');
+                if (newColor === this._borderColor) return;
+
+                this._borderColor = newColor;
+                this._updateBorderStyle();
+            });
+
+            this._alphaChangedId = this._settings.connect('changed::border-alpha', () => {
+                const newAlpha = this._settings.get_double('border-alpha');
+                if (newAlpha === this._borderAlpha) return;
+
+                this._borderAlpha = newAlpha;
+                this._updateBorderStyle();
+            });
+        }
+
+
         // Connect to window added/removed signals
-        this._windowAddedId = global.display.connect('window-created', 
+        this._windowAddedId = global.display.connect('window-created',
             this._onWindowCreated.bind(this));
-        
+
         // Process existing windows
         const windowActors = global.get_window_actors();
         for (const windowActor of windowActors) {
@@ -61,8 +81,16 @@ export default class AlwaysOnTopIndicatorExtension extends Extension {
             this._settings.disconnect(this._settingsChangedId);
             this._settingsChangedId = null;
         }
+        if (this._colorChangedId) {
+            this._settings.disconnect(this._colorChangedId);
+            this._colorChangedId = null;
+        }
+        if (this._alphaChangedId) {
+            this._settings.disconnect(this._alphaChangedId);
+            this._alphaChangedId = null;
+        }
         this._settings = null;
-        
+
         // Disconnect signals
         if (this._windowAddedId) {
             global.display.disconnect(this._windowAddedId);
@@ -73,7 +101,7 @@ export default class AlwaysOnTopIndicatorExtension extends Extension {
         this._borders.forEach((border, metaWindow) => {
             this._removeBorder(metaWindow);
         });
-        
+
         this._handlerIds.forEach((handlers, metaWindow) => {
             if (metaWindow && handlers) {
                 for (const id of Object.values(handlers)) {
@@ -87,13 +115,32 @@ export default class AlwaysOnTopIndicatorExtension extends Extension {
                 }
             }
         });
-        
+
         this._borders.clear();
         this._handlerIds.clear();
     }
 
     _onWindowCreated(display, window) {
         this._setupWindow(window);
+    }
+
+    _getBorderStyle() {
+        const alphaHex = Math.round(this._borderAlpha * 255).toString(16).padStart(2, '0');
+        const color = `#${this._borderColor}${alphaHex}`;
+        return `background-color: ${color};`;
+    }
+
+    _updateBorderStyle() {
+        this._borders.forEach((borderInfo, metaWindow) => {
+            if (borderInfo.actors) {
+                const style = this._getBorderStyle();
+                borderInfo.actors.forEach(actor => {
+                    if (actor) {
+                        actor.set_style(style);
+                    }
+                });
+            }
+        });
     }
 
     _setupWindow(metaWindow) {
@@ -144,7 +191,12 @@ export default class AlwaysOnTopIndicatorExtension extends Extension {
     }
 
     _updateWindowBorder(metaWindow) {
-        if (metaWindow.is_above() && !metaWindow.minimized) {
+        const isAbove = metaWindow.is_above();
+        const isMinimized = metaWindow.minimized;
+        log(`[Always-On-Top] Window: ${metaWindow.get_title()}, is_above: ${isAbove}, minimized: ${isMinimized}`);
+        
+        if (isAbove && !isMinimized) {
+            log(`[Always-On-Top] Adding border to: ${metaWindow.get_title()}`);
             this._addBorder(metaWindow);
         } else {
             this._removeBorder(metaWindow);
@@ -156,23 +208,61 @@ export default class AlwaysOnTopIndicatorExtension extends Extension {
         this._removeBorder(metaWindow);
 
         const windowActor = metaWindow.get_compositor_private();
-        if (!windowActor) return;
+        if (!windowActor) {
+            log(`[Always-On-Top] No window actor for: ${metaWindow.get_title()}`);
+            return;
+        }
 
-        // Create border actor
-        const border = new St.Bin({
+        // Create 4 border actors (top, right, bottom, left) for better control
+        const borderTop = new St.Bin({
             reactive: false,
             can_focus: false,
             track_hover: false,
-            style: `border: ${this._borderWidth}px solid rgba(189, 147, 249, 1);
-                    background-color: transparent;`
+            style: this._getBorderStyle()
+        });
+
+        const borderRight = new St.Bin({
+            reactive: false,
+            can_focus: false,
+            track_hover: false,
+            style: this._getBorderStyle()
+        });
+
+        const borderBottom = new St.Bin({
+            reactive: false,
+            can_focus: false,
+            track_hover: false,
+            style: this._getBorderStyle()
+        });
+
+        const borderLeft = new St.Bin({
+            reactive: false,
+            can_focus: false,
+            track_hover: false,
+            style: this._getBorderStyle()
         });
 
         // Set border size and position
         const updateBorderGeometry = () => {
             try {
                 const rect = metaWindow.get_frame_rect();
-                border.set_position(rect.x - this._borderWidth, rect.y - this._borderWidth);
-                border.set_size(rect.width + 2 * this._borderWidth, rect.height + 2 * this._borderWidth);
+                const bw = this._borderWidth;
+                
+                // Top border
+                borderTop.set_position(rect.x - bw, rect.y - bw);
+                borderTop.set_size(rect.width + 2 * bw, bw);
+                
+                // Right border
+                borderRight.set_position(rect.x + rect.width, rect.y);
+                borderRight.set_size(bw, rect.height);
+                
+                // Bottom border
+                borderBottom.set_position(rect.x - bw, rect.y + rect.height);
+                borderBottom.set_size(rect.width + 2 * bw, bw);
+                
+                // Left border
+                borderLeft.set_position(rect.x - bw, rect.y);
+                borderLeft.set_size(bw, rect.height);
             } catch (e) {
                 // Window may have been destroyed
             }
@@ -180,16 +270,21 @@ export default class AlwaysOnTopIndicatorExtension extends Extension {
 
         updateBorderGeometry();
 
-        // Add to UI group
-        Main.layoutManager.addChrome(border);
+        // Add to UI group with higher stack level
+        Main.layoutManager.addChrome(borderTop, { affectsInputRegion: false });
+        Main.layoutManager.addChrome(borderRight, { affectsInputRegion: false });
+        Main.layoutManager.addChrome(borderBottom, { affectsInputRegion: false });
+        Main.layoutManager.addChrome(borderLeft, { affectsInputRegion: false });
+        
+        log(`[Always-On-Top] Border added successfully for: ${metaWindow.get_title()}`);
 
         // Keep border updated with window geometry
         const sizeChangedId = metaWindow.connect('size-changed', updateBorderGeometry);
         const positionChangedId = metaWindow.connect('position-changed', updateBorderGeometry);
-        
-        // Store border and its handler IDs
+
+        // Store border actors and their handler IDs
         this._borders.set(metaWindow, {
-            actor: border,
+            actors: [borderTop, borderRight, borderBottom, borderLeft],
             sizeChangedId: sizeChangedId,
             positionChangedId: positionChangedId
         });
@@ -214,10 +309,14 @@ export default class AlwaysOnTopIndicatorExtension extends Extension {
                 }
             }
             
-            // Remove border actor
-            if (borderInfo.actor) {
-                Main.layoutManager.removeChrome(borderInfo.actor);
-                borderInfo.actor.destroy();
+            // Remove border actors
+            if (borderInfo.actors) {
+                borderInfo.actors.forEach(actor => {
+                    if (actor) {
+                        Main.layoutManager.removeChrome(actor);
+                        actor.destroy();
+                    }
+                });
             }
             
             this._borders.delete(metaWindow);
--- prefs.js
+++ prefs.js
@@ -1,5 +1,6 @@
 import Gio from 'gi://Gio';
 import Gtk from 'gi://Gtk';
+import Gdk from 'gi://Gdk';
 import Adw from 'gi://Adw';
 
 import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
@@ -20,7 +21,9 @@ export default class AlwaysOnTopIndicatorPreferences extends ExtensionPreference
         });
         page.add(group);
 
-        // Create a spin row for border thickness  
+        const settings = this.getSettings();
+
+        // Border thickness spinner
         const thicknessRow = new Adw.SpinRow({
             title: _('Border Thickness'),
             subtitle: _('Thickness of the border in pixels'),
@@ -29,16 +32,73 @@ export default class AlwaysOnTopIndicatorPreferences extends ExtensionPreference
                 upper: 10.0,
                 step_increment: 0.25,
                 page_increment: 1.0,
-                value: 1.0,
+                value: 2.0,
             }),
-            digits: 2,  // Show two decimal places
+            digits: 2,
             width_chars: 6,
         });
         group.add(thicknessRow);
-
-        // Bind the setting
-        const settings = this.getSettings();
         settings.bind('border-thickness', thicknessRow, 'value',
+            Gio.SettingsBindFlags.DEFAULT);
+
+        // Border color button
+        const colorRow = new Adw.ActionRow({
+            title: _('Border Color'),
+            subtitle: _('Choose the border color'),
+        });
+
+        const colorButton = new Gtk.ColorButton();
+        colorRow.add_suffix(colorButton);
+        group.add(colorRow);
+
+        // Update color button from settings
+        const updateColorButton = () => {
+            const hexColor = settings.get_string('border-color');
+            const rgba = new Gdk.RGBA();
+            rgba.parse('#' + hexColor);
+            colorButton.set_rgba(rgba);
+        };
+        updateColorButton();
+
+        // Connect color button changes
+        colorButton.connect('color-set', () => {
+            const rgba = colorButton.get_rgba();
+            // Convert RGBA to hex format (RRGGBB)
+            const r = Math.round(rgba.red * 255).toString(16).padStart(2, '0');
+            const g = Math.round(rgba.green * 255).toString(16).padStart(2, '0');
+            const b = Math.round(rgba.blue * 255).toString(16).padStart(2, '0');
+            const hexColor = r + g + b;
+            settings.set_string('border-color', hexColor);
+        });
+
+        settings.connect('changed::border-color', () => {
+            updateColorButton();
+        });
+
+        // Border alpha/transparency slider
+        const alphaRow = new Adw.ActionRow({
+            title: _('Transparency'),
+            subtitle: _('0 = Fully transparent, 1 = Fully opaque'),
+        });
+
+        const alphaScale = new Gtk.Scale({
+            orientation: Gtk.Orientation.HORIZONTAL,
+            adjustment: new Gtk.Adjustment({
+                lower: 0.0,
+                upper: 1.0,
+                step_increment: 0.05,
+                page_increment: 0.1,
+                value: 1.0,
+            }),
+            draw_value: true,
+            digits: 2,
+            width_request: 200,
+            hexpand: true,
+        });
+        alphaRow.add_suffix(alphaScale);
+        group.add(alphaRow);
+
+        settings.bind('border-alpha', alphaScale.adjustment, 'value',
             Gio.SettingsBindFlags.DEFAULT);
     }
 }
